49757dc579b3807ba4341392bcfb0771c3e70e22
Jan <110758056+leafboo@users.noreply.github.com>
2024-07-12 13:39:17 +0800

    Make the contents of the description

--- src/Description.tsx
+++ src/Description.tsx
@@ -1,8 +1,11 @@
 export default function Description(){
   return (
     <>
-      <div className='border h-[19rem]'>
-        Description
+      <div className='border h-[19rem] p-11'>
+        <span>&gt; About me</span> <br /><br />
+        <div className=" leading-8">
+          I am Jan Romel Caranguian, I am currently a college student majoring BSIT in the University of Saint Louis Tuguegarao expected to graduate in the year 2026. I mainly focus on web development specifically front end development.
+        </div>
       </div>
     </>
   )
