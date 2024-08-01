463e84b7a389b8eec6b8ccd27a038409002ef7e7
Jan <110758056+leafboo@users.noreply.github.com>
2024-08-01 17:44:40 +0800

    Add links to < TechnologyButtons >

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -11,20 +11,34 @@ export default function Technologies(){
         <br />
         
         <div className=" grid grid-cols-3 gap-3">
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"/>
-          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg"/>
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+                            link="https://en.wikipedia.org/wiki/HTML" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"  
+                            link="https://en.wikipedia.org/wiki/CSS" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+                            link="https://tailwindcss.com" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+                            link="https://en.wikipedia.org/wiki/JavaScript" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+                            link="https://www.typescriptlang.org" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+                            link="https://react.dev" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+                            link="https://vitejs.dev" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg"
+                            link="https://www.php.net" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+                            link="https://www.mysql.com" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+                            link="https://git-scm.com" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg"
+                            link="https://code.visualstudio.com" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"
+                            link="https://www.figma.com" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" 
+                            link="https://www.postman.com" />
+          <TechnologyButton image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg"
+                            link="https://en.wikipedia.org/wiki/Linux" />
         </div>
       </div>
     </>
--- src/components/technologies/TechnologyButton.tsx
+++ src/components/technologies/TechnologyButton.tsx
@@ -1,14 +1,17 @@
 type buttonProps = {
   image: string;
+  link: string;
 }
 
 export default function TechnologyButton(props: buttonProps) {
   return (
     <>
-      <span className="flex justify-center p-4 rounded-[3px] bg-[#2c2a29] cursor-pointer
-                       hover:shadow-[0_0_11px_#77B05F] transition ease-out duration-150">
+      <a className="flex justify-center p-4 rounded-[3px] bg-[#2c2a29] cursor-pointer
+                       hover:shadow-[0_0_11px_#77B05F] transition ease-out duration-150"
+         href={props.link}
+         target="_blank" >
         <img src={props.image} className="w-6" />
-      </span>
+      </a>
     </>
   )
 }
