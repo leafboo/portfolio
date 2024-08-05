d9291ddfc81a2e2e17abeeaa687aedd1a24431fb
Jan <110758056+leafboo@users.noreply.github.com>
2024-08-05 10:27:28 +0800

    Fix alignment of texts

--- src/components/globe/EnterButton.tsx
+++ src/components/globe/EnterButton.tsx
@@ -1,8 +1,8 @@
 export default function EnterButton() {
   return (
     <>
-      <button className="flex justify-center items-center text-[#F4DC53] w-[4rem] border border-[#a5963f] rounded-[.2rem]
-                         text-[1s] bg-[1s]  hover:opacity-90 transition ease-out
+      <button className="flex justify-center items-center text-[#F4DC53] text-[.75rem] w-[4rem] border border-[#a5963f] rounded-[.2rem]
+                         bg-[1s] font-Minecraft hover:opacity-90 transition ease-out
                          duration-150">
         Enter
       </button>
--- src/components/name/OnlineStatus.tsx
+++ src/components/name/OnlineStatus.tsx
@@ -2,7 +2,7 @@ export default function OnlineStatus() {
   return (
     <>
       <span className="text-[#9B0909] text-[1.2rem] font-Minecraft">
-        <span className="text-[4.5rem] absolute bottom-[-.3rem] right-[5rem]">&#183;</span> offline
+        <span className="text-[4.5rem] absolute bottom-[.6rem] right-[5rem]">&#183;</span> offline
       </span>
     </>
   )
